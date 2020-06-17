ccc602472692bac1c9094c0b2ef26e818f3e1160
carter3689 <carter3689@gmail.com>
2020-06-17 01:53:08 -0500

    Layout for search results

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { FormControl, FormGroup, Validators } from '@angular/forms';@Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.scss']
 })
 export class ContactusComponent implements OnInit {
 
+  contactUsForm = new FormGroup({
+    name: new FormControl('',Validators.required),
+    email: new FormControl(''),
+    hearAboutNotes: new FormControl(''),
+    tellaboutyou : new FormControl(''),
+    what_programs: new FormControl('')
+  })
+
   constructor() { }
+
+  getInvolved() {
+    console.log(this.contactUsForm.value)
+    this.contactUsForm.reset()
+    this.contactUsForm.value.markAsPristine();
+    this.contactUsForm.markAsUntouched();
+    this.contactUsForm.updateValueAndValidity();
+  }
 
   ngOnInit(): void {
   }
